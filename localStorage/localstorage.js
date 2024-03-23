3b5ebc8d7d7ede50f96f2d0185505bbd62b5578d
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2024-03-23 04:07:11 -0700

    update localStorage

--- localStorage/localstorage.js
+++ localStorage/localstorage.js
@@ -11,9 +11,17 @@ var jsonData = JSON.stringify(userData);
 // Step 2: Store JSON data in localStorage
 localStorage.setItem('userData', jsonData);
 
-// // Step 3: Retrieve JSON data from localStorage and parse it
+// Step 3: Retrieve JSON data from localStorage and parse it
 var storedData = localStorage.getItem('userData');
 var parsedData = JSON.parse(storedData);
+
+// key(index): Returns the name of the key at the specified index in localStorage.
+let key = localStorage.key(0);
+// console.log(key)
+
+// length: Returns the number of items stored in localStorage.
+let len = localStorage.length;
+// console.log(len)
 
 // --------- destructuring assignment ------------------- 
 // This is a concise way to extract values from an object
@@ -23,6 +31,12 @@ var { name, age, email } = parsedData;
 console.log(name); // Output: John
 console.log(age); // Output: 30
 console.log(email); // Output: john@example.com
+
+// removeItem(key): Removes the key-value pair associated with the specified key from localStorage.
+let removeUser = localStorage.removeItem("userData");
+
+// clear(): Removes all key-value pairs from localStorage.
+localStorage.clear();
 
 // In Google Chrome, you can check the data stored in localStorage.
 // => Open Chrome.
