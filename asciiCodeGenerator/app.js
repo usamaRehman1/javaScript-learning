cd9469c121d1589bb47f7b5af60afe7c438a68ff
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2025-08-26 12:16:52 -0700

    ASCII Code Generator

---
+++ asciiCodeGenerator/app.js
@@ -0,0 +1,5 @@
+let getChar = prompt("Enter character :");
+// gets its Unicode value (ASCII code) using .charCodeAt(0)
+//charCodeAt(0) means "get the character code of the first character".
+let str =  getChar.charCodeAt(0); 
+console.log(str)
